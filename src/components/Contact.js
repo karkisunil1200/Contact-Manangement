7e37a03ce1af59e1f4010b0c1b99714b10ddee77
Sunil Karki <karkisunil1200@gmail.com>
2019-12-28 11:48:51 -0600

    Toggle working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
+  onShowClick = () => {
+    this.setState({showContactInfo: !this.state.showContactInfo});
+  };
+
   render() {
     const {name, email, phone} = this.props;
+    const {showContactInfo} = this.state;
     return (
       <div className='card card-body mb-3'>
-        <h4>{name}</h4>
-        <ul className='list-group'>
-          <li className='list-group-item'>Email: {email}</li>
-          <li className='list-group-item'>Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name} <i onClick={this.onShowClick} className='fas fa-sort-down' />
+        </h4>
+
+        {showContactInfo ? (
+          <ul className='list-group'>
+            <li className='list-group-item'>Email: {email}</li>
+            <li className='list-group-item'>Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
 
 export default Contact;
